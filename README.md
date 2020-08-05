55b080aa80b6b9caf54087966bfba3a7bb0ce75e
tanjimanim555 <53538004+tanjimanim555@users.noreply.github.com>
2020-08-05 14:47:46 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Url-shortener-microservice
-An API project of Freecodecamp.  
+# API Project: URL Shortener Microservice for freeCodeCamp
+
+
+### User Stories
+
+1. I can POST a URL and I will receive a shortened URL in the JSON response. For example `{"original_url":"https://www.google.com","short_url":"bNiTo-IUF"}`
+2. If I pass an invalid URL that doesn't follow the valid `http(s)://www.example.com(/more/routes)` format, the JSON response will contain an error like `{"error":"invalid URL"}`.
+3. When I visit the shortened URL, it will redirect me to my original link.
+
+
+#### Usage:
+
+https://url-shortener-mic.glitch.me/api/shorturl/<shorturl>
---
+++ server.js
@@ -0,0 +1,84 @@
+"use strict";
+
+var express = require("express");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var validUrl = require("valid-url");
+var cors = require("cors");
+
+var app = express();
+var shortId = require("shortid");
+
+// Basic Configuration
+var port = process.env.PORT || 3000;
+
+
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(cors());
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.listen(port, function() {
+  console.log("Node.js listening ...");
+});
+
+const shorturlSchema = mongoose.Schema({
+  originalUrl: String,
+  shortUrl: {
+    type: String,
+    default: shortId.generate
+  }
+});
+
+const ShortUrl = mongoose.model("ShortUrl", shorturlSchema);
+
+app.post("/api/shorturl/new", urlencodedParser, async (req, res) => {
+  var url = req.body.url;
+  if (!validUrl.isWebUri(url)) {
+    res.json({ error: "invalid url" });
+  } else {
+    var newUrl = new ShortUrl({
+      originalUrl: url
+    });
+    try {
+      await newUrl.save();
+    } catch (err) {
+      console.log("error", err);
+      return res.json({
+        error: "failed to store in database"
+      });
+    }
+    res.json({
+      original_url: newUrl.originalUrl,
+      short_url: newUrl.shortUrl
+    });
+  }
+});
+
+app.get("/api/shorturl/:short_url?", async (req, res) => {
+  const smallUrl = req.params.short_url;
+  if (smallUrl === undefined) res.json({ error: "undefined" });
+  else {
+    try {
+      const actualUrl = await ShortUrl.findOne({ shortUrl: smallUrl });
+      if (actualUrl === null) res.json({ error: "wrong short Url entered" });
+      else res.redirect(actualUrl.originalUrl);
+    } catch (error) {
+      console.log("error", error);
+      return res.json({
+        error: "failed to get from database"
+      });
+    }
+  }
+});
